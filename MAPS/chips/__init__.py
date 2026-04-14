55ccba335f936580ca7dc6e635a9f6e96975d22d
xilorem <ivanzaino@gmail.com>
2026-04-14 16:55:06 +0200

    [feature] Added MAGIA chip abstraction.

---
+++ MAPS/chips/__init__.py
@@ -0,0 +1,23 @@
+"""Concrete chip descriptions."""
+
+from .magia import (
+    MAGIA_L1_RESERVED_BYTES,
+    MAGIA_L1_SIZE_BYTES,
+    MAGIA_L1_STACK_BYTES,
+    MAGIA_L1_USABLE_BYTES,
+    MAGIA_L2_SIZE_BYTES,
+    MAGIA_MESH_HEIGHT,
+    MAGIA_MESH_WIDTH,
+    magia_mesh,
+)
+
+__all__ = [
+    "MAGIA_L1_RESERVED_BYTES",
+    "MAGIA_L1_SIZE_BYTES",
+    "MAGIA_L1_STACK_BYTES",
+    "MAGIA_L1_USABLE_BYTES",
+    "MAGIA_L2_SIZE_BYTES",
+    "MAGIA_MESH_HEIGHT",
+    "MAGIA_MESH_WIDTH",
+    "magia_mesh",
+]
---
+++ MAPS/chips/magia.py
@@ -0,0 +1,67 @@
+"""MAGIA chip description."""
+
+from __future__ import annotations
+
+from MAPS.arch import Device, DeviceKind, L1Memory, L2Memory, Mesh, Tile, WorkKind
+
+MAGIA_MESH_WIDTH = 8
+MAGIA_MESH_HEIGHT = 8
+
+MAGIA_L1_SIZE_BYTES = 1024 * 1024
+MAGIA_L1_USABLE_BYTES = 896 * 1024
+MAGIA_L1_STACK_BYTES = 64 * 1024
+MAGIA_L1_RESERVED_BYTES = 64 * 1024
+MAGIA_L2_SIZE_BYTES = 1024 * 1024 * 1024
+
+MAGIA_L2_ACCESS_POINTS = tuple((0, y) for y in range(MAGIA_MESH_HEIGHT))
+
+MAGIA_IDMA_DEVICE = Device(
+    name="idma",
+    kind=DeviceKind.DMA,
+    throughput={WorkKind.DMA: 1.0},
+)
+
+MAGIA_CORE_DEVICE = Device(
+    name="core",
+    kind=DeviceKind.SCALAR,
+    throughput={
+        WorkKind.ELEMENTWISE: 1.0,
+        WorkKind.REDUCE_SUM: 1.0,
+        WorkKind.REDUCE_MAX: 1.0,
+        WorkKind.EXP: 1.0,
+    },
+)
+
+MAGIA_REDMULE_DEVICE = Device(
+    name="redmule",
+    kind=DeviceKind.SYSTOLIC,
+    throughput={WorkKind.GEMM: 192.0},
+)
+
+MAGIA_TILE_DEVICES = (
+    MAGIA_IDMA_DEVICE,
+    MAGIA_CORE_DEVICE,
+    MAGIA_REDMULE_DEVICE,
+)
+
+
+def magia_mesh() -> Mesh:
+    return Mesh(
+        width=MAGIA_MESH_WIDTH,
+        height=MAGIA_MESH_HEIGHT,
+        l2_memory=L2Memory(
+            size=MAGIA_L2_SIZE_BYTES,
+            access_points=MAGIA_L2_ACCESS_POINTS,
+        ),
+        tiles=tuple(
+            Tile(
+                tile_id=y * MAGIA_MESH_WIDTH + x,
+                x=x,
+                y=y,
+                memory=L1Memory(size=MAGIA_L1_SIZE_BYTES),
+                devices=MAGIA_TILE_DEVICES,
+            )
+            for y in range(MAGIA_MESH_HEIGHT)
+            for x in range(MAGIA_MESH_WIDTH)
+        ),
+    )
