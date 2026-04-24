37cff0dc1358dbf2bf843c0d2973225d7afcc3b9
xilorem <ivanzaino@gmail.com>
2026-04-24 14:38:09 +0200

    [refactor] Small cleanup of tile.py

--- MAPS/arch/tile.py
+++ MAPS/arch/tile.py
@@ -6,9 +6,6 @@ from dataclasses import dataclass
 
 from MAPS.arch.device import Device
 from MAPS.arch.memory import L1Memory
-from MAPS.devices.generic import GENERIC_CORE_DEVICE
-
-DEFAULT_TILE_DEVICES = (GENERIC_CORE_DEVICE,)
 
 
 @dataclass(frozen=True)
@@ -18,20 +15,14 @@ class Tile:
     tile_id: int
     x: int
     y: int
-    devices: tuple[Device, ...] = DEFAULT_TILE_DEVICES
-    memory: L1Memory = L1Memory(size=1)
+    memory: L1Memory
+    devices: tuple[Device, ...]
 
     def __post_init__(self) -> None:
+        # check for valid tile identification and mesh position
         if self.tile_id < 0:
             raise ValueError("tile_id must be >= 0")
         if self.x < 0 or self.y < 0:
             raise ValueError("tile coordinates must be >= 0")
         if not self.devices:
-            object.__setattr__(self, "devices", DEFAULT_TILE_DEVICES)
-
-    @property
-    def coords(self) -> tuple[int, int]:
-        return self.x, self.y
-
-    def manhattan_distance(self, other: "Tile") -> int:
-        return abs(self.x - other.x) + abs(self.y - other.y)
+            raise ValueError("tile devices must not be empty")
